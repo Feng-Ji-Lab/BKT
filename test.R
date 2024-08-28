38b4f95faea657b5304cdc24f92efb15457c374a
Yuan Yuhao <325063151@qq.com>
2024-08-29 01:40:58 +0800

    Update test.R

--- test.R
+++ test.R
@@ -1,9 +1,9 @@
 unloadNamespace("BKT");rm(list = ls());devtools::load_all("./");
 library("parallel")
 
-  num_threads <- 16
-    cl <- makeCluster(num_threads)
-model = bkt(seed = 1, num_fits = 1, parallel = FALSE)
-fetch_dataset(model, 'https://raw.githubusercontent.com/CAHLR/pyBKT-examples/master/data/ct.csv', '.')
-fit(model, data_path = 'ct.csv', skills = ".*Plot.*")
-    stopCluster(cl)
+num_threads <- 16
+cl <- makeCluster(num_threads)
+  model = bkt(seed = 1, num_fits = 1, parallel = FALSE)
+  fetch_dataset(model, 'https://raw.githubusercontent.com/CAHLR/pyBKT-examples/master/data/ct.csv', '.')
+  fit(model, data_path = 'ct.csv', skills = ".*Plot.*")
+stopCluster(cl)
