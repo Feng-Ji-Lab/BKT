0b5cfa585055530f7e6e442b5a0a89cde70fd488
Yuan Yuhao <325063151@qq.com>
2024-08-29 03:29:11 +0800

    add api intro

--- goldenCmd.R
+++ goldenCmd.R
@@ -1,6 +1,4 @@
 unloadNamespace("BKT");rm(list = ls());devtools::load_all("./");devtools::test()
 
-现在我实现了一些r里没有的 python 函数，之后要翻译的话就用这里的函数
-matrix.reshape(dim) -> reshape_python(matrix, dim)
 
 devtools::check()
